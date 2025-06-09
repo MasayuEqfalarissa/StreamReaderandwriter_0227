44c54f8204ec8933492e051bf42c5308c17eaec8
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-06-09 17:30:10 +0700

    mendapatkan karakter dalam satu baris

--- iofile.cpp
+++ iofile.cpp
@@ -17,6 +17,8 @@ int main ()
     //unlimited loop untuk meulis 
     while (true)
     {
-        
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris 
+        getline (cin, baris);
     }
  }
