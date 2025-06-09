4b84412f4561771ba4966312f6ba12232f8cb68a
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-06-09 18:49:18 +0700

    add try dan display

--- manualException.cpp
+++ manualException.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main ()
 {
-   
+    try
+    {
+    cout << "selamat belajar di prodi TI UMY" << endl;
+    throw ("halo"); // melemparkan sebuah integar maka 
+    cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
 }
